cf0efcf00c96ceddeb4e8739c34be2c95bf85b7e
kimneskatarzyna@gmail.com <your_github_email@example.com>
2026-05-12 17:04:43 +0100

    Add  class Profile to models.py

--- cards/models.py
+++ cards/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 # Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(
+        User,
+        on_delete=models.CASCADE,
+        related_name='profile'
+    )
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user.username}'s profile"
+    
+
