eed660582852d64ac6a6e5849c622f7964eb4656
kimneskatarzyna@gmail.com <your_github_email@example.com>
2026-05-15 19:43:50 +0100

    Add redirectio from register page to home instead of login

--- accounts/views.py
+++ accounts/views.py
@@ -10,7 +10,7 @@ def register(request):
         if form.is_valid():
             user = form.save()
             login(request, user)
-            return redirect("login")
+            return redirect("home")
     else:
         form = SignUpForm()
         
