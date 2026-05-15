619d6ddf5592ce53ac592ab2e686cd677ebeca23
kimneskatarzyna@gmail.com <your_github_email@example.com>
2026-05-15 17:20:03 +0100

    Add views to register

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from accounts.forms import SignUpForm
+from django.contrib.auth import login
 
 # Create your views here.
 def register(request):
-    return render(request, 'accounts/register.html')
+    if request.method == "POST":
+        form = SignUpForm(request.POST)
+        
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect("login")
+    else:
+        form = SignUpForm()
+        
+    return render(request, 'accounts/register.html', {"form": form})
