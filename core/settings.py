9ceb62575cffe295d9f2df75cb03c23018f675ea
kimneskatarzyna@gmail.com <your_github_email@example.com>
2026-05-20 06:13:52 +0100

    Update the css for displaying the preview picture

--- core/settings.py
+++ core/settings.py
@@ -34,7 +34,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['.herokuapp.com', 'localhost', '127.0.0.1']
 
