f88a29d2023f82fac8a2267ef942f88c340d1293
kimneskatarzyna@gmail.com <your_github_email@example.com>
2026-05-24 07:22:07 +0100

    Create a context_processors to count items in the basket

---
+++ basket/context_processors.py
@@ -0,0 +1,20 @@
+from .models import BasketItem
+
+
+# Context processor to add the count of items in the user's basket to the template context
+# This allows us to display the number of items in the basket in the header or 
+# other parts of the site without having to manually pass this information from every view.
+
+# https://docs.djangoproject.com/en/3.2/_modules/django/template/context_processors/
+
+def basket_item_count(request):
+    if request.user.is_authenticated:
+        count = BasketItem.objects.filter(
+            basket__user=request.user
+        ).count()
+    else:
+        count = 0
+
+    return {
+        "basket_item_count": count
+    }
