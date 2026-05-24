a4746a8a08b08eb4386898e31b908ba51227a7ec
kimneskatarzyna@gmail.com <your_github_email@example.com>
2026-05-24 07:27:05 +0100

    Add link to ref for context_processors

--- basket/context_processors.py
+++ basket/context_processors.py
@@ -6,6 +6,7 @@ from .models import BasketItem
 # other parts of the site without having to manually pass this information from every view.
 
 # https://docs.djangoproject.com/en/3.2/_modules/django/template/context_processors/
+# https://djangocentral.com/how-to-create-custom-context-processors-in-django
 
 def basket_item_count(request):
     if request.user.is_authenticated:
